4aa964e0697baa45ab8daf7ca5eb0b68ae2b3a81
Drincann <1019933576@qq.com>
2022-11-10 02:45:34 +0800

    feat: mongodb connStr config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,12 @@ export default {
   port: 8080,
   host: undefined, // default 127.0.0.1
   env: 'dev',
+  db: {
+    connStr: 'mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority'
+  },
 } as {
-  port: number;
-  host: string | undefined;
-  env: 'dev' | 'prod';
+  port: number
+  host: string | undefined
+  env: 'dev' | 'prod'
+  db: { connStr: string }
 }
