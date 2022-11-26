facfcd9a493fd19edaa30f57aade37d84b473d4f
Drincann <1019933576@qq.com>
2022-11-27 06:42:05 +0800

    fix: 鉴权装饰器 requireManager bug

--- src/api/v1/decorator/requireManager.ts
+++ src/api/v1/decorator/requireManager.ts
@@ -3,7 +3,7 @@ import { verify } from "../../../libs/jwt"
 
 export const requireManager = (api: (...args: any[]) => any) => {
   return async ({ token, ...args }: { token: string }) => {
-    const payload = verify(token)
+    const payload = await verify(token)
     if (payload === null) {
       throw ResponseError.create(errorTypes['verify-error'], { token })
     }
