62f81243bba48ace33bfb585035ca3e82e322a39
Drincann <1019933576@qq.com>
2022-11-27 07:49:07 +0800

    fix: api /auth 签名 await

--- src/api/v1/auth.ts
+++ src/api/v1/auth.ts
@@ -5,6 +5,6 @@ import { sign } from "../../libs/jwt"
 
 export const auth = async ({ code }: { code: string }) => {
   if (code === gencode()) {
-    return { token: sign({}, config.expireTime ?? '30d') }
+    return { token: await sign({}, config.expireTime ?? '30d') }
   } else throw ResponseError.create(errorTypes['auth-error'], { message: `invalid code ${code}` })
 }
