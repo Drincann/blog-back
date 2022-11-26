be679d78d22108d101f5eb8a107de9a205bd72e4
Drincann <1019933576@qq.com>
2022-11-26 13:40:16 +0800

    feat: api /postArticle 使用 responseError

--- src/api/v1/postArticle.ts
+++ src/api/v1/postArticle.ts
@@ -1,14 +1,16 @@
 import { articleColl } from "../../db/client";
 import { Article } from "../../db/types";
+import { errorTypes, ResponseError } from "../../libs/error";
 
 export const postArticle = async ({
   type = 'normal', content, labels = [], title,
 }: Pick<Article, 'type' | 'content' | 'labels' | 'title'>) => {
   if (content === undefined || content === null || title === undefined || title === null) {
-    throw { message: 'content or title is undefined' }
+    throw ResponseError.create(errorTypes['param-error'], { params: 'content, title' })
   }
   const article = await articleColl.insertOne({
     type, content, labels, title,
+    createAt: new Date(), updateAt: new Date(),
   })
   if (article.acknowledged) {
     return (article as any)?.ops?.[0]
